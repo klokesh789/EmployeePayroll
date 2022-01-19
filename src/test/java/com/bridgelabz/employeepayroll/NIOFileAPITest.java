d26fdb9351bce224f5e1d3f8e2dd5487ef72f0ab
lokesh <kottelokesh789@gmail.com>
2022-01-19 15:58:45 +0530

    ADDED: WatchService

--- src/test/java/com/bridgelabz/employeepayroll/NIOFileAPITest.java
+++ src/test/java/com/bridgelabz/employeepayroll/NIOFileAPITest.java
@@ -51,5 +51,12 @@ public class NIOFileAPITest {
 		Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
 				.forEach(System.out::println);
 	}
+	@Test
+	public void givenDirectory_WhenWatched_ShouldReturnAllActivities() throws IOException
+	{
+		Path dir = Paths.get(HOME+'/'+PLAY_WITH_NIO);
+		Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+		new Java8WatchService(dir).processEvents();
+	}
 
 }
